c8e40dd4ea502f2330a7659ab452f8c5ba48ce95
0506kinoshita <irohas.irohas@gmail.com>
2019-11-06 14:48:51 +0900

    文章変更

--- js/modal.js
+++ js/modal.js
@@ -12,11 +12,20 @@ $(function() {
   });
   $("#skillOpen").click(function(){
     $(".skillModal").fadeIn();
+    $("#wrapper").css({
+      filter: 'blur(8px)'
+    })
   });
   $("#repsOpen").click(function(){
     $(".repsModal").fadeIn();
+    $("#wrapper").css({
+      filter: 'blur(8px)'
+    })
   });
   $("#portfolioOpen").click(function(){
     $(".portfolioModal").fadeIn();
+    $("#wrapper").css({
+      filter: 'blur(8px)'
+    })
   });
 });
